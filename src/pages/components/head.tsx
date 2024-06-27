6333c05e7983fa544c4da7197215c447c32e5a14
MFJD <jamisonderilsan1998@gmail.com>
2024-06-27 16:44:19 -0700

    updates

--- src/pages/components/head.tsx
+++ src/pages/components/head.tsx
@@ -159,43 +159,43 @@ const Head = () => {
                                 {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
                                 <DisclosureButton
                                     as="a"
-                                    href="#"
-                                    className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
+                                    href="/"
+                                    className={`${link[1] == undefined || link[1] == "" ? ' border-l-4 border-[#2c7081] bg-gray-50 text-[#2c7081]':''} block  py-2 pl-3 pr-4 text-base font-medium  sm:pl-5 sm:pr-6`}
                                 >
                                     Home
                                 </DisclosureButton>
                                 <DisclosureButton
                                     as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                                    href="/about"
+                                    className={`${link[1] == 'about' ? ' border-l-4 border-[#2c7081] bg-gray-50 text-[#2c7081]':''} block border-l-4  py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6`}
                                 >
                                     About
                                 </DisclosureButton>
                                 <DisclosureButton
                                     as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                                    href="/services"
+                                    className={`${link[1] == 'services' ? ' border-l-4 border-[#2c7081] bg-gray-50 text-[#2c7081]':''} block border-l-4  py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6`}
                                 >
                                     Services
                                 </DisclosureButton>
                                 <DisclosureButton
                                     as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                                    href="/team"
+                                    className={`${link[1] == 'team' ? ' border-l-4 border-[#2c7081] bg-gray-50 text-[#2c7081]':''} block border-l-4  py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6`}
                                 >
                                     Team
                                 </DisclosureButton>
                                 <DisclosureButton
                                     as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                                    href="/news"
+                                    className={`${link[1] == 'news' ? ' border-l-4 border-[#2c7081] bg-gray-50 text-[#2c7081]':''} block border-l-4  py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6`}
                                 >
                                     News
                                 </DisclosureButton>
                                 <DisclosureButton
                                     as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                                    href="/contact"
+                                    className={`${link[1] == 'contact' ? ' border-l-4 border-[#2c7081] bg-gray-50 text-[#2c7081]':''} block border-l-4  py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6`}
                                 >
                                     Contact
                                 </DisclosureButton>
