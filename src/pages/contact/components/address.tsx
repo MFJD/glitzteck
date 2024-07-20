4507d7db1e4d92b7c8628d244eab3ca7b7a074f4
MFJD <jamisonderilsan1998@gmail.com>
2024-07-20 16:03:31 -0700

    updates

--- src/pages/contact/components/address.tsx
+++ src/pages/contact/components/address.tsx
@@ -10,7 +10,7 @@ const Address = () => {
             <div className="mt-10">
                 <p className="text-lg text-gray-600">Need help? call us!</p>
                 <h3 className="text-xl primaryText mt-10">+1 (612) 405-4320</h3>
-                <p className="text-xl mt-10 text-gray-700 ">Adresse : 1400 laurel ave apt w2110 Minneapolis,MN 55403 USA</p>
+                <p className="text-xl mt-10 text-gray-700 ">Adresse : 1400 laurel ave  Minneapolis,MN 55403 USA</p>
                 <p className="text-[20px] mt-5 text-gray-700 ">Email : info@glitzteck.com</p>
             </div>
         </div>
