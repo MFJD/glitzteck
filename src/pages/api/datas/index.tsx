c07b391129637c32ba5a327e920b6d917a1e847a
MFJD <jamisonderilsan1998@gmail.com>
2024-08-01 16:46:32 -0700

    updates

---
+++ src/pages/api/datas/index.tsx
@@ -0,0 +1,92 @@
+export const Datas = [
+    {
+        id: 1,
+        title: 'New Technology',
+        description: 'Download the perfect technology pictures. Find over 100+ of the best free technology images. Free for commercial use No attribution required Copyright-free .',
+        datePosted: '12 August 2024',
+        img: 'https://th.bing.com/th/id/OIP.R5w99e7r3RDTqNfiZe8YrQHaJ4?w=600&h=800&rs=1&pid=ImgDetMain'
+    },
+    {
+        id: 2,
+        title: 'New Technology',
+        description: 'Download the perfect technology pictures. Find over 100+ of the best free technology images. Free for commercial use No attribution required Copyright-free .',
+        datePosted: '12 August 2024',
+        img: 'https://th.bing.com/th/id/R.2aa3cb2b4acfba4d275dcecf01989d93?rik=cpBrJAHxiUJXCg&pid=ImgRaw&r=0'
+    },
+    {
+        id: 3,
+        title: 'New Technology',
+        description: 'Download the perfect technology pictures. Find over 100+ of the best free technology images. Free for commercial use No attribution required Copyright-free .',
+        datePosted: '12 August 2024',
+        img: 'https://th.bing.com/th/id/R.125e16519593a186c48022e5ce64bb77?rik=aCcAPtdlr%2f73bg&riu=http%3a%2f%2fi.ndtvimg.com%2fi%2f2016-05%2fhyundai-hlex_827x510_81463643126.jpg&ehk=POlTa%2bkjSaK%2fltwH%2ft3HLvEQM5h0aOGcslkFAS3gUzc%3d&risl=&pid=ImgRaw&r=0'
+    },
+    {
+        id: 4,
+        title: 'New Technology',
+        description: 'Download the perfect technology pictures. Find over 100+ of the best free technology images. Free for commercial use No attribution required Copyright-free .',
+        datePosted: '12 August 2024',
+        img: 'https://media.istockphoto.com/id/1355632594/photo/high-tech-startup-concept-innovative-male-software-engineer-standing-doing-big-data-analysis.jpg?s=612x612&w=0&k=20&c=fISUO-soPEK5zjw6MS3xR9hcPCYbVDceYPqfSCbhM9E='
+    }
+]
+
+
+
+export const DataOne = [
+    {
+        id: 1,
+        title: 'New Technology',
+        description: 'Download the perfect technology pictures. Find over 100+ of the best free technology images. Free for commercial use No attribution required Copyright-free .',
+        datePosted: '12 August 2024',
+        img: 'https://th.bing.com/th/id/OIP.R5w99e7r3RDTqNfiZe8YrQHaJ4?w=600&h=800&rs=1&pid=ImgDetMain'
+    }
+]
+
+export const posts = [
+    {
+        id: 2,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+        author: {
+            name: 'Lindsay Walton',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    {
+        id: 2,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+        author: {
+            name: 'Lindsay Walton',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    // More posts...
+]
+
+export const featuredPost = {
+    id: 1,
+    title: 'We’re incredibly proud to announce we have secured $75m in Series B',
+    href: '#',
+    description:
+        'Libero neque aenean tincidunt nec consequat tempor. Viverra odio id velit adipiscing id. Nisi vestibulum orci eget bibendum dictum. Velit viverra posuere vulputate volutpat nunc. Nunc netus sit faucibus.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    author: {
+        name: 'Michael Foster',
+        href: '#',
+        imageUrl:
+            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+}
