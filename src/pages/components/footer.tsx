accfcad2e796d0113287f28737fe03ea096118fc
tyannherve11 <tyannherve@gmail.com>
2024-08-09 22:25:00 -0500

    change address

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -149,7 +149,7 @@ export default function Footer() {
                                 <li className="flex">
                                     <i className="ri-map-pin-fill mr-1 text-red-600 text-xl"></i>
                                     <a href="" className="text-lg text-gray-600 hover:text-gray-900">
-                                        1400 Laurel Ave W, Minneapolis, MN 55405, États-Unis
+                                        1117 Marquette Ave, Minneapolis, MN 55403, <USA></USA>
                                     </a>
                                 </li>
                                 <li className="flex">
