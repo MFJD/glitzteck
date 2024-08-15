12ccf2675e261608ba6aa96486b555c9f10d3309
MFJD <jamisonderilsan1998@gmail.com>
2024-08-15 14:50:16 -0700

    updates on services, livechat , news and i have added James's picture on the our team

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -15,22 +15,13 @@ const Team = () => {
             <ScrollToTopButton />
             <div className="w-full justify-center flex my-10">
                 <div className="md:w-[60%] w-full px-4 md:px-0">
-<<<<<<< HEAD
-                    <h3 className="primaryText text-3xl text-center">Our Leadership Team</h3>
-                    <p className="text-center text-gray-600 text-lg textalign">Our leadership team is comprised of incredible humans who want to make the world a better place and improve the lives of their team members.</p>
-=======
                     <h3 className="primaryText text-3xl text-center">Our Team</h3>
                     <p className="text-center text-gray-600 text-lg textalign">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure modi corporis excepturi perferendis sit, expedita ducimus, libero mollitia laudantium, molestias debitis qui. Facilis, repellendus. Praesentium eligendi maiores placeat debitis tenetur?</p>
->>>>>>> 8590616b55bca02049d8e0766822f1f090e3a125
                 </div>
             </div>
             <div className="mx-auto max-w-6xl px-6 pb-2 pt-18 sm:pt-10 lg:px-8 lg:pt-20">
                 <Users />
-<<<<<<< HEAD
-                <CTA/>
-=======
                 <CTA />
->>>>>>> 8590616b55bca02049d8e0766822f1f090e3a125
             </div>
             <Footer />
         </motion.div>
