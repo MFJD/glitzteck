17d4dc374f7de19a0272e5769da77f5d51e9092d
tyannherve11 <tyannherve@gmail.com>
2024-09-10 22:00:08 -0500

    news page

--- src/pages/api/datas/index.tsx
+++ src/pages/api/datas/index.tsx
@@ -3,29 +3,29 @@
 export const posts = [
     {
         id: 2,
-        title: 'Boost your conversion rate',
+        title: 'Here are some tips about data protection:',
         href: '#',
         description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        date: 'Mar 10, 2020',
-        datetime: '2020-03-16',
+            '- Use strong, unique passwords and consider a password manager. - Be cautious with links and attachements from unknown sources. - Use encryption for sensitive data. - Use VPN (Virtual Private Network) on public Wi-Fi.',
+        date: 'Jul 10, 2024',
+        datetime: '2024-07-10',
         author: {
-            name: 'Lindsay Walton',
+            name: 'Yann Tchoumta',
             href: '#',
             imageUrl:
-                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            <img src="/images/users/tchoumtayann.png" alt="" />
         },
     },
     {
         id: 2,
-        title: 'Boost your conversion rate',
+        title: 'Tech news',
         href: '#',
         description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        date: 'Mar 10, 2020',
-        datetime: '2020-03-16',
+            'Apple has also incorporated AI technology into their latest Watch, AirPods and iPhone models.',
+        date: 'Sep 10, 2024',
+        datetime: '2024-09-10',
         author: {
-            name: 'Lindsay Walton',
+            name: 'tech news',
             href: '#',
             imageUrl:
                 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -36,14 +36,14 @@ export const posts = [
 
 export const featuredPost = {
     id: 1,
-    title: 'We’re incredibly proud to announce we have secured $75m in Series B',
+    title: 'The challenges of tehnology in Africa',
     href: '#',
     description:
-        'Libero neque aenean tincidunt nec consequat tempor. Viverra odio id velit adipiscing id. Nisi vestibulum orci eget bibendum dictum. Velit viverra posuere vulputate volutpat nunc. Nunc netus sit faucibus.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
+        'The challenges of technology in Africa include infrastructure limitations, such as unreliable electricity and internet connectivity, hindering tech adoption. Additionally, low digital literacy and tech skills among the population, limited funding and investment for tech startups, and weak regulatory frameworks create obstacles for tech innovation. Cybersecurity threats also pose a significant risk due to limited expertise and resources. Despite these challenges, Africa has made significant progress in tech adoption and innovation. However, the continent still faces issues like brain drain, limited local content and services, and dependence on international tech solutions. Furthermore, tech access and benefits often exacerbate existing social and economic inequalities. Addressing these challenges is crucial to unlocking Africa full tech potential and promoting inclusive and sustainable digital growth.',
+    date: 'Sep 10, 2024',
+    datetime: '2024-09-10',
     author: {
-        name: 'Michael Foster',
+        name: 'Yann Tchoumta',
         href: '#',
         imageUrl:
             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
