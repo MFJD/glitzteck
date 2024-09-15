d29f8a130441be45e1d41d21b862550251819de3
MFJD <jamisonderilsan1998@gmail.com>
2024-09-15 22:35:33 +0100

    updates

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -6,7 +6,10 @@ import ScrollToTopButton from "@/components/scrollButton";
 import Users from "./components/users";
 import CTA from "../components/Cta";
 import OurTeam from "./components/teams";
+import { useTranslation } from 'react-i18next';
+
 const Team = () => {
+    const { t} = useTranslation();
     return (
         <motion.div
             initial={{ opacity: 0, y: 0 }}
@@ -16,11 +19,8 @@ const Team = () => {
             <ScrollToTopButton />
             <div className="w-full justify-center flex my-10">
                 <div className="md:w-[60%] w-full px-4 md:px-0">
-                    <h3 className="primaryText text-3xl text-center">The entrepreneurs behind
-                    the vision.</h3>
-                    <p className="text-center text-gray-600 text-lg textalign">Get to know the 
-                        people behind GlitzTeck and discover how their expertise and dedication 
-                        are shaping the future of technology and connectivity.</p>
+                    <h3 className="primaryText text-3xl text-center">{t('theader1')}</h3>
+                    <p className="text-center text-gray-600 text-lg textalign">{t('tbody1')}</p>
                 </div>
             </div>
             <div className="mx-auto max-w-6xl px-6 pb-2 pt-18 sm:pt-10 lg:px-8 lg:pt-20">
